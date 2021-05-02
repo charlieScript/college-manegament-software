bbe462c25b7f5016d133c27c9f661c243a5f2b0b
charlieScript <gozione@gmail.com>
2021-05-02 03:05:11 +0100

    added few stuff

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+
+app.set()
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`server statted at port ${PORT}`));
+
